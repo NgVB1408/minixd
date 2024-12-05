759a5aa73fd90d9784670c3e593952bee9f3383e
Vinny <176841602+NgVB1408@users.noreply.github.com>
2024-12-05 19:16:26 +0700

    Update telegramApi.js

--- bot/utils/telegramApi.js
+++ bot/utils/telegramApi.js
@@ -1,1 +1,17 @@
+const axios = require('axios');
 
+const sendMessage = async (chatId, text) => {
+  const token = '<YOUR_TELEGRAM_BOT_TOKEN>';
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+  try {
+    await axios.post(url, {
+      chat_id: chatId,
+      text: text,
+    });
+  } catch (error) {
+    console.error('Error sending message:', error);
+  }
+};
+
+module.exports = { sendMessage };
